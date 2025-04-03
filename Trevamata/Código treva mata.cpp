d4f319c20562730a2d49cfa9699d0a157bd563db
ScoobyTT <christianamosa@gmail.com>
2025-04-03 19:51:26 -0300

    Update Código treva mata.cpp
    Resposta final e correta.

--- Trevamata/Código treva mata.cpp
+++ Trevamata/Código treva mata.cpp
@@ -1,63 +1,46 @@
-
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
-
-// 
-struct fase {
-    int f;    // Número da fase
-    int exp;  // Experiência da fase
-    int bonus; // Bônus da fase
+struct  anao{
+    string nome;
+    int peso;
 };
 
 int main() {
-    vector<fase> fases; // Vetor para armazenar as fases
-    fase aux;
+    int N;
+    cin >> N;
 
-    int n, d, e, f = 0;
+    vector<string> nomes;
+    vector<anao> anoes;
+    anao aux;
 
-    // Leitura do número de fases c
-    cout << "Digite o número de fases cumpridas: ";
-    cin >> n;
-
-    // Leitura do número 
-    cout << "Digite o número de cada fase: " << endl;
-    for (int i = 0; i < n; i++) {
-        cin >> aux.f;
-        fases.push_back(aux);
+    for (int i = 0; i < N; ++i) {
+        cin >> aux.nome >> aux.peso;
+        anoes.push_back(aux);
     }
 
-    // Leitura da experiência 
-    cout << "Digite a quantidade de exp em cada fase: " << endl;
-    for (int i = 0; i < n; i++) {
-        cin >> fases[i].exp;
+    int capacidade;
+    cin >> capacidade;
+    bool cmp = false;
+
+    for(int i = 0; i < N; i++){
+        if(anoes[i].peso > capacidade){
+            cmp = true;
+            nomes.push_back(anoes[i].nome);
+        }
+    }
+    if(cmp == false){
+        cout << "Vamos todos encontrar a montanha!" << endl;
+    }else{
+        cout << "Vamos virar almoço de aranhas gigantes!" << endl;
+        for(int i = 0; i < nomes.size(); i++){
+            cout << nomes[i] << endl;
+        }
     }
 
-    // Leitura do bônus 
-    cout << "Digite a quantidade de bônus de cada fase: " << endl;
-    for (int i = 0; i < n; i++) {
-        cin >> fases[i].bonus;
-    }
-
-    // Leitura da experiência necessária para subir de nível e do bônus adicional
-    cout << "Digite a experiência necessária para subir de nível: ";
-    cin >> d;
-    cout << "Digite o bônus adicional: ";
-    cin >> e;
-
-    // Cálculo da pontuação total (experiência + bônus)
-    for (int i = 0; i < n; i++) {
-        f += fases[i].exp + fases[i].bonus;
-    }
-    f += e; // Adiciona o bônus adicional
-
-    // Verificacao
-    if (f >= d) {
-        cout << "Upou de Nivel!" << endl;
-    } else {
-        cout << "Nao foi dessa vez!" << endl;
-    }
+   
 
     return 0;
-}
+} 
