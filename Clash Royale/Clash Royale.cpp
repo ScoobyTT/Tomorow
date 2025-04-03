53bae6254fe126cefcd84fe10feb0699122c937f
ScoobyTT <christianamosa@gmail.com>
2025-04-03 20:15:26 -0300

    Update Clash Royale.cpp
    resposta certa

--- Clash Royale/Clash Royale.cpp
+++ Clash Royale/Clash Royale.cpp
@@ -6,40 +6,41 @@ using namespace std;
 
 int main() {
     int n;
-    cin >> n;  // Corrigido de ciin para cin
+    cin >> n;
 
     queue<int> fila;
     queue<int> fil;
     
-    // Corrigido o loop para inserir os valores na fila
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         int j;
         cin >> j;
         fila.push(j);
     }
-
+    
     int l;
-    cin >> l;  // Corrigido de cin l; para cin >> l;
-
-    // Corrigido o loop para inserir os valores na segunda fila
-    for(int i = 0; i < l; i++) {
+    cin >> l;
+    
+    for (int i = 0; i < l; i++) {
         int j;
         cin >> j;
         fil.push(j);
     }
-
-    // Corrigido a lógica de comparação entre as filas
-    if(fila.size() >= fil.size()) {
-        int a = fila.front();  // Corrigido para acessar corretamente o front
-        fila.pop();             // Corrigido para usar o pop() corretamente
-        fila.push(a);           // Corrigido para usar o push() corretamente
-        cout << a << endl;
-    } else {
-        int cemiterio = fil.front();  // Corrigido para acessar corretamente o front
-        fil.pop();                    // Corrigido para usar o pop() corretamente
-        fil.push(cemiterio);          // Corrigido para usar o push() corretamente
-        cout << cemiterio << endl;
+    
+    int c = 0;
+    
+    while (!fila.empty() && !fil.empty()) {
+        if (fila.front() >= fil.front()) {
+            int a = fila.front();
+            fila.pop();
+            fila.push(a);
+            fil.pop();
+        } else {
+            fila.pop();
+            fil.pop();
+            c++;
+        }
     }
-
+    
+    cout << c << endl;
     return 0;
 }
